d5b8361baefb3496f2d29de393add24bf0d172d6
mukultyagi1 <tyagimukul14@gmail.com>
2023-12-07 09:58:52 +0000

    nodejs changes

---
+++ srv/cat-service.js
@@ -0,0 +1,18 @@
+const cds = require('@sap/cds');
+const axios = require("axios");
+module.exports = async (srv) => {
+    srv.before('CREATE', 'Employees', async function (req) {
+ 
+       
+           
+        debugger;
+            if (req.data.EmployeeID == false) {
+                req.error({ code: 418, message: 'ID is Mandatory' });
+            }
+ 
+       
+ 
+ 
+ 
+    });
+}
